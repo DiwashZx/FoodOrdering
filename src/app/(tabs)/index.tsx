f2497f33eb28b225d3a5fac19faa23bc6c072928
DiwashZx <diwashjoshi345@gmail.com>
2024-02-03 15:54:52 +0545

    Type safe components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
 import {  View } from "react-native";
-import products from "../../../assets/data/products";
-import ProductListItem from "../../components/ProductListitem";
+import products from "@assets/data/products";
+import ProductListItem from "@components/ProductListitem";
 
 
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[3]} />
-      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[8]} />
+      <ProductListItem product={products[6]} />
       <ProductListItem product={products[2]} />
     </View>
   );
--- src/components/ProductListitem.tsx
+++ src/components/ProductListitem.tsx
@@ -1,18 +1,28 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import Colors from "../constants/Colors";
+import { Product } from "../types";
 
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
 
-const ProductListItem = ({ product  }) => {
+type ProductListitemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListitemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
 
-    export default ProductListItem;
+export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
